7a65d398359df2b33ef75eb3f8d4302100123402
Ali Madooei <madooei@jhu.edu>
2020-11-25 11:04:22 -0500

    Step 4

--- src/CreateMeeting.js
+++ src/CreateMeeting.js
@@ -1,5 +1,20 @@
+import "./CreateMeeting.css";
+
 function CreateMeeting() {
-  return <div>Create Meeting</div>;
+  return (
+    <div>
+      <a href="/" className="close-create-meeting">Close</a>
+      <form className="create-meeting-form">
+        <div className="create-meeting-details">
+          <input type="text" name="course" placeholder="Course" />
+          <input type="text" name="instructor" placeholder="Instructor" />
+          <input type="text" name="time" placeholder="Meeting Times" />
+          <input type="url" name="link" placeholder="Meeting Link" />
+          <button>Add Meeting</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateMeeting;
