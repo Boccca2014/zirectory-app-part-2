6aaefce1bb166ca284ec5616baa54353d629a61d
Ali Madooei <madooei@jhu.edu>
2020-11-25 12:05:05 -0500

    Step 5

--- src/CreateMeeting.js
+++ src/CreateMeeting.js
@@ -1,20 +1,35 @@
+import { Component } from "react";
 import "./CreateMeeting.css";
 
-function CreateMeeting() {
-  return (
-    <div>
-      <a href="/" className="close-create-meeting">Close</a>
-      <form className="create-meeting-form">
-        <div className="create-meeting-details">
-          <input type="text" name="course" placeholder="Course" />
-          <input type="text" name="instructor" placeholder="Instructor" />
-          <input type="text" name="time" placeholder="Meeting Times" />
-          <input type="url" name="link" placeholder="Meeting Link" />
-          <button>Add Meeting</button>
-        </div>
-      </form>
-    </div>
-  );
+class CreateMeeting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: "",
+      instructor: "",
+      time: "",
+      link: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="/" className="close-create-meeting">
+          Close
+        </a>
+        <form className="create-meeting-form">
+          <div className="create-meeting-details">
+            <input type="text" name="course" placeholder="Course" />
+            <input type="text" name="instructor" placeholder="Instructor" />
+            <input type="text" name="time" placeholder="Meeting Times" />
+            <input type="url" name="link" placeholder="Meeting Link" />
+            <button>Add Meeting</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateMeeting;
